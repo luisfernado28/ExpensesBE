9ade55a2672d2c34e07976a05db3addd113094db
Luis Fernando Choque <luisfer_2810@outlook.com>
2025-08-18 17:00:56 -0400

    corsupdate

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000") 
+            policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://34.239.42.92:5000") 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
