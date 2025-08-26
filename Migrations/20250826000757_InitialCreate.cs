23855746165da3bff5757c34bc501b9953d4b5e8
Luis Fernando Choque <luisfer_2810@outlook.com>
2025-08-25 20:10:37 -0400

    Migration added

---
+++ Migrations/20250826000757_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ExpensesBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "expenses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    purchase = table.Column<string>(type: "text", nullable: false),
+                    purchasedate = table.Column<DateOnly>(type: "date", nullable: true),
+                    timestamp = table.Column<DateOnly>(type: "date", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: false),
+                    amount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_expenses", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "expenses");
+        }
+    }
+}
